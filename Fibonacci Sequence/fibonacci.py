99d61dffad82b47025fa094a91b06caf73e52142
Adam Twigge <79292264+Twiggecode@users.noreply.github.com>
2021-06-30 00:09:52 +0100

    Update fibonacci.py

--- Fibonacci Sequence/fibonacci.py
+++ Fibonacci Sequence/fibonacci.py
@@ -2,15 +2,22 @@
 
 
 def fibonacci(n):
+    n += 1;
     i=1
     num1,num2=0,1
-    while i<n-1:
-        if n==1:
-            return 0
-        sum=num1+num2
-        num1,num2=num2,sum
-        i += 1
-    return sum
+    
+    if n==1:
+            return 0;       
+    elif n==2:
+        return 1; 
+    else:
+    
+        while i<n-1:
+        
+            sum=num1+num2
+            num1,num2=num2,sum
+            i += 1
+        return sum
 
 my_num=int(input("Enter a number:"))
-print(f"The {my_num}th element of fibonacci series is:",fibonacci(my_num))
+print(f"The {my_num}th element of fibonacci series is:",fibonacci(my_num)) 
