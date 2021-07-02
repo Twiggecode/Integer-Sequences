5eefbf35fafd168fc4a983e50ce1470d77c26722
Jordan <70343977+Trianglium@users.noreply.github.com>
2021-07-02 08:56:03 -0500

    Created Python Script, manual_catalan_numbers.py
    manual_catalan_numbers.py does NOT use python's math module (it manually calculates factorials).

---
+++ Catalan Numbers/manual_catalan_numbers.py
@@ -0,0 +1,19 @@
+""" Catalan Numbers (sequence A000108 in the OEIS) """
+""" (2n)! / ((n + 1)!n!) """
+# The first ten  (starting with n=0 are: 1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862)
+
+
+
+def Catalan_Formula(n):
+    formula = Fact(2*n) / (Fact(n + 1) * Fact(n))
+    return('C{} = {}'.format(n, formula))
+
+
+def Fact(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact = fact * i
+    return(fact)
+
+
+print(Catalan_Formula(n = int(input("Enter a Number: "))))
