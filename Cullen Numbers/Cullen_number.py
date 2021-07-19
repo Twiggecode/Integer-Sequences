7366d21add9e63ed7f0b71e67927515f67e61724
Bogdan Ciurea <ciureabogdanalexandru@yahoo.com>
2021-07-19 13:51:03 +0100

    :sparkles: Python for Cullen Numbers

---
+++ Cullen Numbers/Cullen_number.py
@@ -0,0 +1,16 @@
+
+def cullen_number(n):
+    return n * (2 ** n) + 1
+
+if __name__ == "__main__":
+    
+    while True:
+        number = int(input("Enter a number: "))
+
+        if(number > -1):
+            break
+		
+        else:
+            print("Natural number expected!")
+	
+    print(cullen_number(number))
