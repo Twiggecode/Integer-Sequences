65859f1608333ea0e741454ae345707b7a7dac8c
suzanSarandon <filgalanos@gmail.com>
2021-07-22 13:09:16 +0300

    Added the recaman sequence

---
+++ Recaman's Sequence/recaman_sequence.py
@@ -0,0 +1,28 @@
+import itertools
+sequence_array = []
+
+def recaman(a:int):
+
+    
+    if a == 0:
+        sequence_array.append(a)
+    elif a > 0 and sequence_array[a-1]-a>0 and sequence_array[a-1]-a not in sequence_array:
+        sequence_array.append(sequence_array[a-1]-a)
+    else:
+        sequence_array.append(sequence_array[a-1]+a)
+    return sequence_array
+
+
+
+if __name__ == "__main__":
+    n = int(input("Enter integer value: "))
+    if n < 0:exit("Error input. Please enter a non-negative integer value!")
+    gen = (recaman(x) for x in range(n+1))
+    index = n
+    res_arr = (next(itertools.islice(gen, index, None)))
+    res = res_arr[index]
+    print(res)
+
+
+
+    #print(next(gen))
