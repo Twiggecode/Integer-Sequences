e8f74b3893957d33bd04beeaad733aae3cbef132
Feeshboi <79869508+Feeshboi@users.noreply.github.com>
2021-07-22 12:13:43 +0200

    Update to reflect indexing starting at zero
    Previously script functioned by taking inputs 1,2,3 as 1st, 2nd, 3rd values in Woodall numbers.
    Update means 0 is first term, 1is second term etc. as per Readme

--- Woodall Numbers/Woodall_Numbers.py
+++ Woodall Numbers/Woodall_Numbers.py
@@ -4,6 +4,6 @@
 
 #Formula: n*2n − 1, with n ≥ 1.
 #Example : {1, 7, 23, 63, 159, 383, 895, 2047, 4607, ...}
-
+#Input of 0 for n = 1, 1 for n = 2...
 find_val = int(input("Enter the nth value : "))
-print(find_val,"th value of Woodall Number is ",find_val*(2**find_val)-1)
+print(find_val,"th value of Woodall Number is ",(find_val+1)*(2**(find_val+1))-1)
