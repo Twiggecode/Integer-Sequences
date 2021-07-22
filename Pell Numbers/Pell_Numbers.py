c8aaa4cffee7c2f0a843960d34930240429be9db
PremApk <a.premapk99@gmail.com>
2021-07-22 15:11:10 +0530

    Added Pell Numbers...

---
+++ Pell Numbers/Pell_Numbers.py
@@ -0,0 +1,16 @@
+#################################
+###     Pell Numbers          ###
+#################################
+
+def pellNumbers(n):
+        if(n <= 2):
+                return n
+        first = 1
+        second= 2
+        for i in range(3, n+1):
+                tmp = 2 * second + first
+                first, second = second, tmp
+        return second
+
+find_val = int(input("Enter the nth value : "))
+print(find_val,"th value in Pell Number is ",pellNumbers(find_val))
