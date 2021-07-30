7d14f729ba8a4061e0ef04485d2edef639879625
Aseem Chopra <aseemchopra@protonmail.com>
2021-07-30 09:48:41 +0530

    ✨ Added C++ for Jacobsthal Numbers

--- Jacobsthal Numbers/JacobsthalNumbers.cpp
+++ Jacobsthal Numbers/JacobsthalNumbers.cpp
@@ -6,7 +6,7 @@ using namespace std;
 #Example:  0, 1, 1, 3, 5, 11, 21, 43, 85, 171, 341, 683, 1365, 2731, 5461, 10923, 21845, 43691, ……*/
 
 int jacob(int n){
-    n=n-1;
+    n=n-1; // As first term is 0
     int dp[n+1]={0};
     dp[0]=0;
     dp[1]=1;
