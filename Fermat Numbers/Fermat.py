196dcea5282700b507d45c8b0c6a8e16344af073
Adam Twigge <79292264+Twiggecode@users.noreply.github.com>
2021-08-03 18:34:15 +0100

    Update Fermat.py

--- Fermat Numbers/Fermat.py
+++ Fermat Numbers/Fermat.py
@@ -1,14 +1,44 @@
-import math as Math
+# Iterative Function to calculate (x^y) in O(log y)
+def power(x, y):
+    res = 1  # Initialize result
 
-# this code returns the nth element of the Fermat numbers where n is a positive integer (or zero) input by the user
+    while (y > 0):
 
-# the loop is used to check if the number input by the user is a positive integer or zero
-n = int()
-while True:
-    n = int(input("Enter a number: "))
-    if n > -1:
-        break
-    print("Invalid number, please enter a new number.")
+        # If y is odd,
+        # multiply x with the result
+        if (y & 1):
+            res = res * x
 
-print(round(Math.pow(2, Math.pow(2, n)) + 1))
-# calculate the Fermat numbers using Fn = 2^(2^n)  + 1
+        # n must be even now
+        y = y >> 1  # y = y/2
+        x = x * x  # Change x to x^2
+    return res
+
+
+# Function to find nth fermat number
+def Fermat(i):
+    # 2 to the power i
+    power2_i = power(2, i)
+
+    # 2 to the power 2^i
+    power2_2_i = power(2, power2_i)
+
+    return power2_2_i + 1
+
+
+# Function to find first n Fermat numbers
+def Fermat_Number(n):
+    for i in range(n):
+
+        # Calculate 2^2^i
+        print(Fermat(i), end="")
+
+        if (i != n - 1):
+            print(end=", ")
+
+
+if __name__ == '__main__':
+    n = int(input("Enter the nth value to find : "))
+
+    # Function call
+    Fermat_Number(n)
