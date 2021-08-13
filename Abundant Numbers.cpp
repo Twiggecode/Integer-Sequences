6baefc49e4f7ba736817c0ff7c2fb1e7268c5f8f
geekboi <85869106+geekboi777@users.noreply.github.com>
2021-08-13 07:38:10 +0530

    Abundant Numbers.cpp
    An optimised solution for printing the first n Abundant numbers

---
+++ Abundant Numbers.cpp
@@ -0,0 +1,53 @@
+// An Optimized Solution to check Abundant Number
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Function to calculate sum of divisors
+int getSum(int n)
+{
+    int sum = 0;
+ 
+    // Note that this loop runs till square root
+    // of n
+    for (int i=1; i<=sqrt(n); i++)
+    {
+        if (n%i==0)
+        {
+            // If divisors are equal,take only one
+            // of them
+            if (n/i == i)
+                sum = sum + i;
+ 
+            else // Otherwise take both
+            {
+                sum = sum + i;
+                sum = sum + (n / i);
+            }
+        }
+    }
+ 
+    // calculate sum of all proper divisors only
+    sum = sum - n;
+    return sum;
+}
+ 
+// Function to check Abundant Number
+bool checkAbundant(int n)
+{
+    // Return true if sum of divisors is greater
+    // than n.
+    return (getSum(n) > n);
+}
+ 
+/* Driver program to test above function */
+int main()
+{   int n ; cin >> n;
+    for(int i =0; i <=n ;i++){
+        if (checkAbundant(i)){
+             cout << (i)<< ' ';
+    }   
+}
+        
+    
+    return 0;
+}
