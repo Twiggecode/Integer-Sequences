db5249ced770d299e481a317015078097af7b6b6
haswanth89 <66584863+haswanth89@users.noreply.github.com>
2021-08-30 12:46:29 +0530

    Update PowerOf2.cpp
    This is a simplified powerof2 code using bitwise operator.

--- Powers Of 2/PowerOf2.cpp
+++ Powers Of 2/PowerOf2.cpp
@@ -1,25 +1,15 @@
 #include<bits/stdc++.h>
+#define long long ll
 using namespace std;
-
-
-int FastPowerOf2(int n){  // returns nth power of 2 using Bit Manipulation
-	int x =2;
-	int res =1;
-	while(n>0){
-		if(n&1 != 0){
-			res = res * x;
-		}
-		x *= x;
-		n = n>>1;
-	}
-	return res;
-}
 
 int main(){
 	int n;
 	cout<<"Enter the power of 2 : ";
 	cin >> n;
-	cout<<n<<"to the Power of 2 is : "<<FastPowerOf2(n);
+	//we can directly use the left shift operator to calculate 2^n.
+	//This is a bitwise operator.
+	//we can ll to convert to long long.
+	cout<<n<<"to the Power of 2 is : "<<(1ll<<n);
 	return 0;
 }
 
