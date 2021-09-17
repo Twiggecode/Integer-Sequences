39fb4b2ee9dd9819b117c4ac3117accaf67a3441
Sedatyf <gaxel@hotmail.fr>
2021-09-17 15:07:41 +0200

    Add try/catch to Carol Numbers in C#

--- Carol Number/carol_number.cs
+++ Carol Number/carol_number.cs
@@ -5,20 +5,35 @@ namespace IntegerSequences.CarolNumbers
     class carol_number
     {
         // The first few Carol numbers are: −1, 7, 47, 223, 959, 3967, 16127, 65023, 261119, 1046527 (sequence A093112 in the OEIS).
-        private static double carolNumber(int n)
+        private static double Carol(int n)
         {
-            n++;
+            n++; // if n == 0 then returns a wrong value
             return (Math.Pow(Math.Pow(2, n) - 1, 2)) - 2;
         }
 
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Please, enter an integer: ");
-            string input = Console.ReadLine();
-            int nTH = int.Parse(input);
+            Console.WriteLine("Please enter an integer n where n >= 0");
+            bool isValid = false;
+            int nTH = 0;
 
-            Console.WriteLine(carolNumber(nTH));
+            while (!isValid) // Check if user input an integer. If not, tries again until an integer is inputted 
+            {
+                try
+                {
+                    string input = Console.ReadLine();
+                    nTH = int.Parse(input);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter an integer n where n >= 0");
+                    continue;
+                }
+                isValid = true;
+            }
+            String s = String.Format("The {0}th element from Carol numbers is {1}", nTH + 1, Carol(nTH));
+            Console.WriteLine(s);
         }
     }
 }
