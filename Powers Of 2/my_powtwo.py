7b95371d25333cfd5c9889bcaad5448a6cc50ddc
itsf4llofstars <irooted4hal@mailfence.com>
2021-09-21 13:54:47 -0500

    print series function

--- Powers Of 2/my_powtwo.py
+++ Powers Of 2/my_powtwo.py
@@ -2,15 +2,30 @@
 
 
 def pow_two() -> int:
-    return lambda m: 2 ** m
+    return lambda n: 2 ** n
+
+
+def pow_series(end: int) -> None:
+    series = pow_two()
+    for i in range(0, end):
+        print(series(i))
 
 
 def main():
+    stop = 10
+    pow_series(stop)
+
+    my_power_of_two = pow_two()
+    result = my_power_of_two(16)
+    print(result)
+
     foo = pow_two()
-    print(foo(2))
-    print(foo(8))
-    print(foo(16))
-    print(foo(64))
+    result = foo(16)
+    print()
+    print(result)
+    result /= 2
+    result -= 1
+    print(int(result))
 
 
 if __name__ == '__main__':
