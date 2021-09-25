2bbdc6ba6128eaa885c894318c62418a7fe68fe0
itsf4llofstars <irooted4hal@mailfence.com>
2021-09-24 23:03:19 -0500

    Write final code and functions

--- Deficient Numbers/defnum.py
+++ Deficient Numbers/defnum.py
@@ -1,39 +1,66 @@
 #!/usr/bin/env python3
-"""Python script to save a series of numbers, print the
-series of numbers provided the numbers are deficient
+"""Python script to save, as a list, a series of numbers that
+are deficient. Numbers are deficient if sum(m) < 2 * m, whith sum(m) being
+the summation of m's divisors. Scripts gives the option to print the list of
+deficient nubmers.
 """
 
 
-def print_deficients(begin: int, end: int) -> None:
-    if type(begin) != int or type(end) != int:
-        raise ValueError("\nThe begin and the end number must be integers\n")
+def print_deficients(begin: int, end: int) -> int:
+    """print_deficients(begin, end) -> int:
 
-    deficient_nums = []
+    Calculates if a series of number between begin, and end, [inclusive] are
+    deficient and appends the deficient number to a list
+
+    parameters:
+        begin: [int]: First number to calculate
+        end: [int]: Last number to calculate
+    returns:
+        deficient_nums: list[int]: List of deficient numbers calculated
+    """
+    deficient_nums: list[int] = []
 
     while begin <= end:
+        # We stop iterating half-way to begin and since begin is a divisor of
+        # begin we set divisors variable to begin
         divisors: int = begin
 
         index: int = 1
+        # Iterate to half of begin since numbers greater than (begin / 2) will
+        # not be divisors
         while index <= begin // 2:
-            if begin % index == 0:
-                divisors += index
+            if begin % index == 0:  # Checks if divisor
+                divisors += index  # sums divisor if i is a divisor
+
             index += 1
 
-        if divisors < (2 * begin):
-            deficient_nums.append(begin)
-            # print(f"{begin} is deficient")
+        if divisors < 2 * begin:  # Checks for deficiency
+            deficient_nums.append(begin)  # Appends to list if deficient
 
-        begin += 1
+        begin += 1  # Increment one number closer to end variable
 
     return deficient_nums
 
-def main():
-    # start = int(input("Enter start number: "))
-    # stop = int(input("Enter stop number: "))
-    start, stop = 10, 20
+
+def check_if_deficient():
+    """check_fi_dificient():
+    TODO: Future implimentation.
+    """
+    pass
+
+
+def main() -> None:
+    # Gets users start and stop value. Raises ValueError if number is not an int
+    start = int(input("Enter start number: "))
+    stop = int(input("Enter stop number: "))
+    # start, stop = 940, 950
+
     deficients = print_deficients(start, stop)
-    print()
-    [print(number) for number in deficients]
+
+    #  Query's user if they wish to print the returned list of dificient nubers
+    if str(input("Enter 'y' to print the dificient numbers: ").lower()) == 'y':
+        print()
+        [print(f"{number} is deficient") for number in deficients]
 
 
 if __name__ == '__main__':
