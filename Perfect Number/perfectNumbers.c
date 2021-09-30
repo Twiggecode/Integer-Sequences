093b256bea779e1c003c269f711ed0f2c1f15285
itsf4llofstars <irooted4hal@mailfence.com>
2021-09-30 11:30:26 -0500

    fixed code and functions
    Change comments to reflect correct explanations of perfect numbers
    Fixed functions to perform computations correctly

--- Perfect Number/perfectNumbers.c
+++ Perfect Number/perfectNumbers.c
@@ -1,52 +1,88 @@
 /**
  * Algorithms to show, return and check if a number is a
  * Perfect Number. A perfect number, sum(m) == m (excludeing m)
- * sum(m) is the summation of m's divisors, is a number whos
- * even divisors sum to the number (m). The number (m) is not
+ * is the summation of m's divisors, is a number whos even
+ * divisors sum to the number (m). The number (m) is not
  * included in the summation
  */
 
 // TODO: Comment code
 #include <stdio.h>
 
-void printPerfects(int, int);
-int checkIfPerfect(int);
-// void nthPerfectNumber(int);
+// Function definitions
+void printPerfects(long, long);
+long checkIfPerfect(long);
+// void nthPerfectNumber(long);
 
 int main() {
+    // Code to show results of functions
     printPerfects(0, 9000);
 
-    // int isItPerfect = 8128;
-    // int itsPerfect = checkIfPerfect(isItPerfect);
+    puts("");
 
-    // if (itsPerfect)
-    //     printf("%d is Perfect\n", isItPerfect);
-    // else
-    //     printf("%d is not Perfect\n", isItPerfect);
+    long isItPerfect = 8128;
+    long perfectNumber = checkIfPerfect(isItPerfect);
+
+    if (perfectNumber)
+        printf("%li is Perfect\n", isItPerfect);
+    else
+        printf("%li is not Perfect\n", isItPerfect);
+
+    puts("");
+
+    isItPerfect = 12345;
+    perfectNumber = checkIfPerfect(isItPerfect);
+
+    if (perfectNumber)
+        printf("%li is Perfect\n", isItPerfect);
+    else
+        printf("%li is not Perfect\n", isItPerfect);
 
     return 0;
 }
 
-void printPerfects(int start, int stop) {
+/**
+ * Prints all perfect number from start to stop
+ */
+void printPerfects(long start, long stop) {
+    // Iterate form star to stopt
     while (start <= stop) {
-        int divisors = 0;
+        // Set divisors summation value to 0
+        long divisors = 0;
 
-        for (int i = 1; i < (start / 2) + 1; ++i)
+        // Check for perfect divisors, add to divisors if found
+        // Stop half way to current start value as no perfect
+        // divisors exist past the half of start value
+        for (long i = 1; i <= (start / 2) + 1; ++i)
             divisors += (start % i == 0) ? i : 0;
 
+        // Check if divisors sum is equal to the start value
+        // print if it is true
         if (divisors == start)
-            printf("%d is Perfect\n", start);
+            printf("%li is Perfect\n", start);
 
+        // Goto the next number
         ++start;
     }
 }
 
-int checkIfPerfect(int n) {
-    int divisors = n;
+/**
+ * Checks if the passed number is a perfect number
+ * returns 1 if true 0 if false
+ */
+long checkIfPerfect(long n) {
+    // Set divisors summation to 0
+    long divisors = 0;
 
-    for (int i = 1; i < (n / 2) + 1; ++i)
+    // Iterate through all possible perfect divisors.
+    // Check if it is a perfect divisor.
+    // Add the divisor to divisor variable if it is perfect.
+    // Stop half was to the passed number as there are no
+    // perfect divisors beyond this point.
+    for (long i = 1; i <= (n / 2) + 1; ++i)
         divisors += (n % i == 0) ? i : 0;
 
-    return (divisors == 2 * n) ? 1 : 0;
+    // return 1 if perfect else return 0
+    return (divisors == n) ? 1 : 0;
 }
 
