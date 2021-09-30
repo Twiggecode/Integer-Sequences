2ce699b59c9f0d978a85bc4c93b5c03619c8ca80
itsf4llofstars <irooted4hal@mailfence.com>
2021-09-30 12:45:39 -0500

    nth prime number function

--- Perfect Number/perfectNumbers.c
+++ Perfect Number/perfectNumbers.c
@@ -1,26 +1,30 @@
 /**
  * Algorithms to show, return and check if a number is a
- * Perfect Number. A perfect number, sum(m) == m (excludeing m)
- * is the summation of m's divisors, is a number whos even
- * divisors sum to the number (m). The number (m) is not
- * included in the summation
+ * Perfect Number. A perfect number, sum(n) == n (excludeing n)
+ * is the summation of n's divisors, is a number whos even
+ * divisors sum to the number (n). The number (n) is not
+ * included in the summation.
+ * These algorithsm are unable to calculate past the 5th
+ * perfect number as such a number exceedes the size of a
+ * long integer.
  */
 
 // TODO: Comment code
+#include <math.h>
 #include <stdio.h>
 
 // Function definitions
 void printPerfects(long, long);
-long checkIfPerfect(long);
-// void nthPerfectNumber(long);
+long checkIfPerfect(long); // Requires a positive number greater than 1.
+void nthPerfectNumber(int);
 
 int main() {
     // Code to show results of functions
-    printPerfects(0, 9000);
+    printPerfects(0, 10000);
 
     puts("");
 
-    long isItPerfect = 8128;
+    long isItPerfect = 33550336;
     long perfectNumber = checkIfPerfect(isItPerfect);
 
     if (perfectNumber)
@@ -30,13 +34,17 @@ int main() {
 
     puts("");
 
-    isItPerfect = 12345;
+    isItPerfect = 33550335;
     perfectNumber = checkIfPerfect(isItPerfect);
 
     if (perfectNumber)
         printf("%li is Perfect\n", isItPerfect);
     else
         printf("%li is not Perfect\n", isItPerfect);
+
+    puts("");
+
+    nthPerfectNumber(4);
 
     return 0;
 }
@@ -57,8 +65,9 @@ void printPerfects(long start, long stop) {
             divisors += (start % i == 0) ? i : 0;
 
         // Check if divisors sum is equal to the start value
-        // print if it is true
-        if (divisors == start)
+        // print if it is true.
+        // By definition perfect numbers are greater than 1.
+        if ((divisors == start) && (start > 1))
             printf("%li is Perfect\n", start);
 
         // Goto the next number
@@ -84,5 +93,24 @@ long checkIfPerfect(long n) {
 
     // return 1 if perfect else return 0
     return (divisors == n) ? 1 : 0;
+}
+
+void nthPerfectNumber(int n) {
+    int numPerfectNums = 0;
+    long checkNum = 0;
+
+    while (numPerfectNums < n) {
+        long divisors = 0;
+
+        ++checkNum;
+
+        for (long i = 1; i <= (checkNum / 2) + 1; ++i)
+            divisors += (checkNum % i == 0) ? i : 0;
+
+        if ((divisors == checkNum) && (checkNum > 1))
+            ++numPerfectNums;
+    }
+
+    printf("The %i-th perfect number is %li\n", n, checkNum);
 }
 
