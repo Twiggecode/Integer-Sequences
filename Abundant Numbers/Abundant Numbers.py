f146d68dde7d56decae511b65b6a4637068b4c29
Sergiy Fomin <sergiy.fomin@sourcedgroup.com>
2021-10-06 15:45:19 -0400

    updated formatting: indentation, functions names, and added a runner function to wrap the main func

--- Abundant Numbers/Abundant Numbers.py
+++ Abundant Numbers/Abundant Numbers.py
@@ -1,32 +1,38 @@
 import math
 
-def getSum(n):
-        sum = 0
-        i = 1
-        while( i <= (math.sqrt(n))):
-                if(n%i == 0):
-                        if( n/i == i):
-                                sum += i
-                        else:
-                                sum += i
-                                sum = sum + (n / i)
-                i += 1
-        sum = sum - n
-        return sum
+def get_sum(n: int) -> int:
+    sum = 0
+    i = 1
 
-def isAbundant(n):
-        if(getSum(n) > n):
-                return True
-        return False
-
-n = int(input("Enter the nth value to find : "))
-count = 0
-ans = []
-i = 0
-while(n >= count):
-        if(isAbundant(i)):
-                ans.append(i)
-                count += 1
+    while i <= math.sqrt(n):
+        if n % i == 0:
+            if n / i == i:
+                sum += i
+            else:
+                sum += i
+                sum = sum + (n / i)
         i += 1
-print(n,"th value of Abundant Number is ",ans[n])
-        
+    sum = sum - n
+    return sum
+
+def is_abundant(n: int) -> bool:
+    if get_sum(n) > n:
+        return True
+    return False
+
+def find_nth_value() -> None:
+    n = int(input("Enter the nth value to find: "))
+    count = 0
+    ans = []
+    i = 0
+
+    while n >= count:
+        if is_abundant(i):
+            ans.append(i)
+            count += 1
+        i += 1
+
+    print(f"The value of Abundant Number is {ans[n]}")
+
+if __name__ == "__main__":
+    find_nth_value()
