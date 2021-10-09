5d13cc590474e09fa2245503b509bc13ef7c6cdb
ShyamSundaram <mail.shyam.sundaram@gmail.com>
2021-10-09 22:59:25 +0530

    Modified Python code implementing Memoization

--- Tribonacci Sequence/Tribonacci.py
+++ Tribonacci Sequence/Tribonacci.py
@@ -1,37 +1,20 @@
 # This code will return the nth element of the Tribonacci sequence
 
+def tribonacci(n,mem={}):
+    if(n==0 or n==1):
+        mem[n]=0
+        return mem[n]
+    elif(n==2 or n==3):
+        mem[n]=1
+        return mem[n]
 
-def user_input():
-    n = int(input("Enter the value of n: "))
-    n = n + 1
-    return n
+    if(n in mem.keys()):
+        return mem[n]
 
-
-def tribonacci():
-    # Initialize the first three sequence values
-    a, b, c = 0, 1, 1
-
-    n = user_input()
-
-    if n == 1:  # return the 0th element of the sequence for input=0
-
-        return 0
-
-    elif (
-        n == 2 or n == 3
-    ):  # the next two elements of the sequence are both 1, so return 1 for input=1 and input=2
-
-        return 1
-
-    else:  # else we use the equation Tn = Tn-1 + Tn-2 + Tn-3
-
-        for i in range(n - 3):
-            d = a + b + c
-            a = b
-            b = c
-            c = d
-        return d
-
+    mem[n]=tribonacci(n-3)+tribonacci(n-2)+tribonacci(n-1)
+    return mem[n]
+    
 
 if __name__ == "__main__":
-    print(f"The Nth term in the sequence is {tribonacci ()}")
+    n=int(input("Enter the value of n: "))
+    print(f"The Nth term in the sequence is {tribonacci (n,)}")
