80760aec1bf0b06490be6b5b30193d88a9f72055
Emmet Allen <59685332+Emmet-Allen@users.noreply.github.com>
2021-10-19 02:12:25 -0400

    Collatz Conjecture in C
    Collatz Conjecture (3n + 1 Number Sequence) written in C

--- Collatz Conjecture/CollatzConjecture.c
+++ Collatz Conjecture/CollatzConjecture.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
- int* collatz(int *n){
+int* collatz(int *n){
     if(*n % 2 == 0){
-      *n /= 2; 
-     }
+        *n /= 2; 
+    }
     else if(*n % 2 == 1){
-      *n = 3 * (*n) + 1; 
+        *n = 3 * (*n) + 1; 
     }
     return n; 
 }
 
- int main(void){
-     int num = 0, counter = 1;
-     printf("Enter a number greater than 0: ");
-     scanf("%d", &num);
+int main(void){
+    int num = 0, counter = 1;
+    printf("Enter a number greater than 0: ");
+    scanf("%d", &num);
     
     while(num != 1){
         counter++;
@@ -21,4 +21,4 @@
         printf("This is collatz conjecture at step %d = %d \n", counter, num);
     }
     return 0; 
- }
+}
