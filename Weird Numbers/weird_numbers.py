8fc5f08f7a260c7d6c7c5ab92f3bdeee59b41e91
Sterling Abrahams <43232009+gunslingster@users.noreply.github.com>
2021-10-23 23:41:53 -0700

    Update weird_numbers.py

--- Weird Numbers/weird_numbers.py
+++ Weird Numbers/weird_numbers.py
@@ -12,7 +12,6 @@ def find_divisors(n):
                 divisors.extend([i,int(n/i)])
         i += 1
     divisors.remove(n)
-    divisors.sort()
     return divisors
 
 def subset_generator(arr):
@@ -21,15 +20,13 @@ def subset_generator(arr):
             yield elem
 
 def isWeird(num):
-    isSemiPerfect = False
-    isAbundant = False
     divisors = find_divisors(num)
-    if sum(divisors) > num:
-        isAbundant = True
+    if not (sum(divisors) > num):
+        return False
     for subset in subset_generator(divisors):
         if sum(subset) == num:
-            isSemiPerfect = True
-    return isAbundant and not isSemiPerfect
+            return False
+    return True
 
 def main():
     n = int(input('Enter n: '))
@@ -42,12 +39,6 @@ def main():
     print(curr)
 
 main()
-
-
-
-
-
-
 
 
 
