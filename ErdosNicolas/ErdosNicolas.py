1f95544f80a4d4ab1aa77658af53d8f3220bbb9a
Sterling Abrahams <43232009+gunslingster@users.noreply.github.com>
2021-11-01 10:21:32 -0700

    Update more efficient
    Removed some unnecessary code and made more efficient.

--- ErdosNicolas/ErdosNicolas.py
+++ ErdosNicolas/ErdosNicolas.py
@@ -1,33 +1,37 @@
+# Erdos Nicolas Numbers
+import math
+
 def find_divisors(n):
     divisors = []
-    for i in range(1, n//2 + 1):
+    i = 1
+    while i <= math.sqrt(n):
         if n % i == 0:
-            divisors.append(i)
+            if i == n/i:
+                divisors.append(i)
+            else:
+                divisors.extend([i, int(n//i)])
+        i += 1
+    divisors.sort()
     return divisors
 
-def isPerfect(n):
-    divisors = find_divisors(n)
-    sum_divisors = sum(divisors)
-    if sum_divisors == n:
-        return True, divisors
-    else:
-        return False, divisors
-
-def isErdosNicols(n):
-    perfect, divisors = isPerfect(n)
-    if perfect:
-        return False
-    for i in range(1, len(divisors)):
-        if sum(divisors[0:i+1]) == n:
+def erdos_nicolas(num):
+    divisors = find_divisors(num)
+    for i in range(0, len(divisors)-2):
+        if sum(divisors[0:i+1]) == num:
             return True
     return False
 
-n = int(input('Enter n: '))
-count = -1
-current = 0
-while count < n:
-    current += 1
-    if isErdosNicols(current):
-        count += 1
-print(current)
+def main():
+    n = int(input('Enter n: '))
+    count = -1
+    current = 0
+    while count < n:
+        current += 1
+        if erdos_nicolas(current):
+            count += 1
+    print(current)
+
+if __name__ == '__main__':
+    main()
+            
 
