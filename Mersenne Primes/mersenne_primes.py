f9bed0e32760248e499d05833c31c2fbb84656ea
Sterling Abrahams <43232009+gunslingster@users.noreply.github.com>
2021-11-04 19:51:49 -0700

    Return the nth Mersenne prime.
    Not sure if this is the fastest but it is pretty efficient.

---
+++ Mersenne Primes/mersenne_primes.py
@@ -0,0 +1,36 @@
+import math
+
+def isPrime(num):
+    if num <= 3:
+        return num > 1
+    if num % 2 == 0:
+        return False
+    if num % 3 == 0:
+        return False
+    i = 6
+    while i <= math.sqrt(num):
+        if num % (i-1) == 0 or num % (i+1) == 0:
+            return False
+        i += 6
+    return True
+
+def powers_of_two():
+    p = 1
+    while True:
+        p = 2 * p
+        yield p
+
+def main():
+    n = int(input('Enter n: '))
+    count = -1
+    g = powers_of_two()
+    while count < n:
+        curr = next(g)
+        if isPrime(curr - 1):
+            count += 1
+    print(curr - 1)
+
+if __name__ == '__main__':
+    main()
+    
+    
