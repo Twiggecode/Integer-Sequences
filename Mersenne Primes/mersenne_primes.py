0c1dc71d16eee789fdc12b955445126fbcaa9b51
Sterling Abrahams <43232009+gunslingster@users.noreply.github.com>
2021-11-06 10:44:57 -0700

    FIx to isPrime function.
    isPrime fixed. Before it was not incrementing correctly. It was returning True for numbers like 121.

--- Mersenne Primes/mersenne_primes.py
+++ Mersenne Primes/mersenne_primes.py
@@ -7,9 +7,9 @@ def isPrime(num):
         return False
     if num % 3 == 0:
         return False
-    i = 6
+    i = 5
     while i <= math.sqrt(num):
-        if num % (i-1) == 0 or num % (i+1) == 0:
+        if num % (i) == 0 or num % (i+2) == 0:
             return False
         i += 6
     return True
