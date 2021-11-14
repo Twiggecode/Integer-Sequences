a73e31c88366d1ef221b10582c9473eedcab0a4a
Sterling Abrahams <43232009+gunslingster@users.noreply.github.com>
2021-11-14 14:47:08 -0800

    Add original implementation
    The previous implementation used the library function, which does not provide value to the repository. 
    
    This implementation returns the nth element from the positive indexed Euler numbers. This is OEIS A000364. I chose this sequence because the original sequence has every odd entry as 0. It also has alternating negative entries. Implementing these wouldn't serve much purpose. 
    
    It works off of the definition that the nth Euler number is the number of downup permutations of [2n].

--- Euler Numbers/euler.py
+++ Euler Numbers/euler.py
@@ -1,5 +1,31 @@
-from scipy.special import euler
+import math
+from itertools import permutations
 
-n = int(input("Input: "))
+def alternating(arr):
+    breakpoint()
+    i = 0
+    flag = 0
+    while i < len(arr) - 1:
+        if flag:
+            if arr[i+1] > arr[i]:
+                flag = not flag
+            else:
+                return False
+        else:
+            if arr[i+1] < arr[i]:
+                flag = not flag
+            else:
+                return False
+        i += 1
+    return True
 
-print(round(euler(n)[-1]))
+def downup_permutations(n):
+    p = list(permutations([i for i in range(1, n+1)], n))
+    downup = filter(alternating, p)
+    return list(downup)
+
+def euler(n):
+    return len(downup_permutations(2*n))
+
+n = int(input('Enter n: '))
+print(euler(n))
