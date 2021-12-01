215b7214f0e999181ad8cebdd6936f2abf47c878
Goutham232 <katpallygoutham123@gmail.com>
2021-12-01 12:36:16 +0530

    create square_numbers.java

---
+++ Square Numbers/square_numbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class square_numbers {
+    private static int nth_square_value(int n) {
+        
+        return ( (n-1) + 1 ) * ( (n-1) + 1 );
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the nth value to find :");
+        int n = sc.nextInt();
+        System.out.println(n + "th value in square numbers sequence is " + nth_square_value((n)));
+        sc.close();
+    }
+}
