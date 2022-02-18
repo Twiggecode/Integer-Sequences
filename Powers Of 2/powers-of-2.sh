afbb058744ae780e1af3ca74356068390cf585ba
itsf4llofstars <irooted4hal@mailfence.com>
2022-02-17 21:54:07 -0600

    Lint script

--- Powers Of 2/powers-of-2.sh
+++ Powers Of 2/powers-of-2.sh
@@ -67,7 +67,7 @@ exit 0
 #: Documentation for man page creation.
 #: Create man page by:
 #: Filling in the sections below with your code and text. Don't touch the lines with the equals
-#: sign. Run the pod2man command below these lines in the command prompt. The extension on the 2nd 
+#: sign. Run the pod2man command below these lines in the command prompt. The extension on the 2nd
 #: script line should be 1.
 
 #: $ pod2man powers-of-two.sh > powers-of-two.1
