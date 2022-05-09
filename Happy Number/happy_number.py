b85f3a7b4964bccbe26ed0a1dc42b07636fe8915
Roei klein <roei.klein@post.ac.idc.il>
2022-05-09 18:28:16 +0300

    Improve quality of existing code
    Improve quality of existing code - Happy number

--- Happy Number/happy_number.py
+++ Happy Number/happy_number.py
@@ -4,27 +4,43 @@
 This algorithm determines if a number n is happy number.
 '''
 
-def main():
-    n = int(input("Enter a number : "))
 
-    def recursion(n):
-        rem = 0
-        total = 0
-        # Loop to square each digit and then add them
-        while (n != 0):
-            rem = n % 10
-            n = n // 10
-            total += rem**2
-        return total
+# This algorithm determines if a number is happy number.
+def isHappy(n: int) -> bool:
+    def get_next(number):
+        total_sum = 0
+        while number > 0:
+            number, digit = divmod(number, 10)
+            total_sum += digit ** 2
+        return total_sum
 
-    while True:
-        if(recursion(n) == 1):
-            print("True")
-            return True
-        n = recursion(n)
-        if (n < 10):
-            print("False")
-            return False
+    while n != 1 and n != 4:
+        n = get_next(n)
+
+    return n == 1
+
+# This algorithm returns the Nth happy number.
+def happy(n: int) -> int:
+    count, index = 0, 1
+    while count <= n:
+        if isHappy(index):
+            count += 1
+        index += 1
+
+    return index - 1
+
+
+def test(happy):
+    assert happy(0) == 1
+    assert happy(1) == 7
+    assert happy(2) == 10
+    assert happy(3) == 13
+    assert happy(4) == 19
+    assert happy(5) == 23
+    assert happy(6) == 28
+    assert happy(7) == 31
+    assert happy(8) == 32
+
 
 if __name__ == "__main__":
-    main()
+    test(happy)
