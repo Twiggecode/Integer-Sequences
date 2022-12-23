0a6bd9f4a1d141de415fe0509652a02a13cd3b5e
Mohd Abbas Haidar <102041201+psibarpsi@users.noreply.github.com>
2022-12-23 17:48:06 +0530

    Making it run in O (log2 n) instead of O (2^n).
    (1) Instead of using recursion, Fibonacci (int n) function is edited to use matrix exponentiation algorithm which would convert it's time complexity from O(2^n) 
         (exponential) to O(log2 n) (logarithmic).
    (2) Some links to resources explaining matrix exponentiation algorithm have been added.
    (3) Some formatting changes have been done to make the code look cleaner.

--- Fibonacci Sequence/Fibonacci.cpp
+++ Fibonacci Sequence/Fibonacci.cpp
@@ -4,23 +4,75 @@ using namespace std;
 // example of Fibonacci sequence ------ 0, 1, 1, 2, 3, 5, 8, 13....
 // 1st and 2nd term = 0 , 1 respectively and proceeding terms are sum of previous two terms 
 
-int Fibonacci(int i){   // Fibonacci function expects index of the sequence
-						// and returns the term present at the index
-	if(i == 0 or i == 1){
-		return i;
-	}
-	else{
-		return Fibonacci(i-1) + Fibonacci(i-2) ;
-	}
+const int N = 1e9 + 7;
+
+// Multiplying the parameter matrices:
+vector<vector<int>> multiply (vector <vector <int>> a, vector <vector <int>> b)
+{
+    vector <vector <int>> r (2, vector <int> (2, 0));
+    for (int i = 0; i < 2; ++i)
+    {
+        for (int j = 0; j < 2; ++j)
+        {
+            for (int k = 0; k < 2; ++k)
+            {
+                r[i][j] += a[i][k] * b[k][j];
+                r[i][j] %= N;
+            }
+        }
+    }
+    
+    return r;
 }
 
-int main(){
-	int n; // n is the expected user input
-	cout<<"Enter the required index of Fibonacci sequence : ";
-	cin >> n ;
-	cout<<"The Term at required index of Fibonacci sequence is : ";
-	cout<<Fibonacci(n);
-return 0;
+// Finding T^n:
+void matrixExponentiation (vector <vector <int>> &T, int n)
+{
+    // defining the identity matrix:
+    vector <vector <int>> I = {{1, 0},
+                               {0, 1}};
+    vector <vector <int>> current = T;
+    T = I;
+    while (n > 0)
+    {
+        if (n%2 != 0)
+        {
+            T = multiply (T, current);
+        }
+        
+        n = n/2;
+        current = multiply (current, current);
+    }
 }
 
+int Fibonacci (int n)
+{
+    // *** for details of matrix exponentiation method visit: ***
+    // (1) https://www.youtube.com/watch?v=k43lupRL8jU
+    // (2) https://www.youtube.com/watch?v=Y-slDsjyxmY
+	
+    if (n == 1)	return 0;
+    if (n == 2) return 1;
+	
+    n -= 2;
+	
+    // computing nth Fibonacci number using matrix exponentiation algorithm:
+ 
+    // Transition matrix:
+    vector <vector <int>> T = {{0, 1},
+                               {1, 1}};
+	
+    matrixExponentiation (T, n);
+	
+    return T[1][1];
+}
 
+int main()
+{
+    int n; // n is the expected user input
+    cout << "Enter the required index of Fibonacci sequence : ";
+    cin >> n;
+    cout << "The Term at required index of Fibonacci sequence is : "; 
+    cout << Fibonacci (n);
+    return 0;
+}
